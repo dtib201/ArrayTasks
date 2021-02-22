0a65c0d6abf8fda44edf1f3d739ff1c49bfd7ea1
Daniel Min Tibbotts <dtib201@mywhitecliffe.com>
2021-02-23 09:47:57 +1300

    Initial Commit
    Created initial Visual Studio files.

---
+++ ArrayTasks/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ *   ARRAY TASKS 
+ *   Daniel Tibbotts
+ *   23/02/2021
+ *   
+ */
+
+using System;
+
+namespace ArrayTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
